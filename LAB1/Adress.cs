36a5d57c51e250f49bfd6b26745790aa3185abfc
Kamila Tałaj <w64227@student.wsiz.edu.pl>
2023-03-06 14:15:13 +0100

    add new models

---
+++ LAB1/Adress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    public class Address
+    {
+        public string Number { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ LAB1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public DateTime Birthday { get; set; }
+            public Address Address { get; set; }
+        }
+ 
+}
